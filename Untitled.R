23e6dc7aaf822bd98bf9d11b2dfc2eab0102267c
Faria Ishika <ishika.f21@gmail.com>
2018-07-03 15:49:55 -0700

    First script to count number of tumor/normal samples in TCGA LUAD dataset

--- Untitled.R
+++ Untitled.R
@@ -1,2 +1,39 @@
 #Projects
 #after projects
+test2<-ifelse(test1<10,'tumor','normal')
+test2<-ifelse(test3<10,'tumor','normal')
+#test4<-ifelse(test3<10,'tumor','normal')
+> test5<-num [1:26]
+
+#1-26 - Tumors
+# setNames(data.frame(as.list(1:5)), LETTERS[1:5])
+
+data.frame(c1=c('a','b'),c2=c(1,2)) 
+26-36
+
+#out of 46 - 1 is normal -38
+substr(x, start, stop)
+substr(x, 13, stop)
+
+column_names <- colnames(expression_data)
+nchar(column_names)
+substr(column_names, 1, 1)
+column_names <- colnames(expression_data)
+
+column_names
+
+substr(column_names, 14, 15)
+
+#as.numeric- to turn the number into numeric
+as.numeric(column_names, 14, 15)
+substr(column_names, 14, 15)
+a <- substr(column_names, 14, 15)
+a
+test13<-ifelse(a<10,'tumor','normal')
+view
+
+
+#test 13 - result stored
+
+#as.data.frame(table(test13))
+#counting vector ^^
